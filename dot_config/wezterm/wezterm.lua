de3dde4c2556bb6cbc08a3ec67539584c84750fb
Matias Duhalde <contact@duhal.de>
2024-12-08 20:30:04 -0300

    chore: add CJK fallback fonts

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,8 +1,10 @@
+local font = require("font")
 local theme = require("theme")
 local launch = require("launch")
 
 local config = {}
 
+font.apply_to_config(config)
 theme.apply_to_config(config)
 launch.apply_to_config(config)
 
